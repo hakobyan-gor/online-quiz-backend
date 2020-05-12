e09d44a87d25279aae2ba45f5ae356a1988aa76d
Gor <gorhakobyann@gmail.com>
2020-05-12 19:33:14 +0400

    Swagger config with jwt auth

--- src/main/java/com/quiz/security/config/swagger/SwaggerConfig.java
+++ src/main/java/com/quiz/security/config/swagger/SwaggerConfig.java
@@ -1,12 +1,17 @@
 package com.quiz.security.config.swagger;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.spi.DocumentationType;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.service.*;
+import com.google.common.collect.Lists;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -15,10 +20,47 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-          .select()
-          .apis(RequestHandlerSelectors.any())
-          .paths(PathSelectors.any())
-          .build();
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.quiz.rest.controllers"))
+                .paths(PathSelectors.any())
+                .build()
+                .securitySchemes(Lists.newArrayList(apiKey()))
+                .securityContexts(Lists.newArrayList(securityContext()))
+                .apiInfo(apiInfo());
+    }
+
+    @Bean
+    public SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.any())
+                .build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Lists.newArrayList(
+                new SecurityReference("JWT", authorizationScopes));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "Header");
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Online Quiz",
+                null,
+                "v1",
+                null,
+                null,
+                null,
+                null,
+                Collections.emptyList()
+        );
     }
 
 }
