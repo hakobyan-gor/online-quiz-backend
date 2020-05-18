792561a564d518788d2fbf4e2f1c6086966713eb
Gor <gorhakobyann@gmail.com>
2020-05-18 12:48:07 +0400

    not special changes

--- src/main/java/com/quiz/rest/controllers/AnswerController.java
+++ src/main/java/com/quiz/rest/controllers/AnswerController.java
@@ -5,15 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.quiz.models.response.ResponseModel;
 import com.quiz.rest.services.AnswerService;
-import javax.ws.rs.core.MediaType;
 import com.quiz.models.Answer;
 import java.util.List;
-import javax.ws.rs.*;
 
-@Produces({ MediaType.APPLICATION_JSON })
-@Consumes(MediaType.APPLICATION_JSON)
 @Api(value = "Answer Controller")
-@Path("/answers")
+@RequestMapping("/answers")
 @RestController
 public class AnswerController {
 
@@ -24,33 +20,28 @@ public class AnswerController {
         this.answerService = answerService;
     }
 
-    @Path("/add-list")
-    @POST
-    public ResponseModel<List<Answer>> createQuestions(List<Answer> answerList){
+    @PostMapping("/add-list")
+    public ResponseModel<List<Answer>> createQuestions(@RequestBody List<Answer> answerList){
         return answerService.createAnswers(answerList);
     }
 
-    @Path("/add")
-    @POST
-    public ResponseModel<Answer> createQuestion(Answer answer){
+    @PostMapping("/add")
+    public ResponseModel<Answer> createQuestion(@RequestBody Answer answer){
         return answerService.createAnswer(answer);
     }
 
-    @Path("/get/question/{id}")
-    @GET
-    public ResponseModel<List<Answer>> getAnswersByQuestionId(@PathParam("id") Long id){
+    @GetMapping("/get/question/{id}")
+    public ResponseModel<List<Answer>> getAnswersByQuestionId(@PathVariable Long id){
         return answerService.getAnswersByQuestionId(id);
     }
 
-    @Path("/get/{id}")
-    @GET
-    public ResponseModel<Answer> getAnswer(@PathParam("id") Long id){
+    @GetMapping("/get/{id}")
+    public ResponseModel<Answer> getAnswer(@PathVariable Long id){
         return answerService.getAnswerById(id);
     }
 
-    @Path("/update/{id}")
-    @PUT
-    public ResponseModel<Answer> updateAnswer(@PathParam("id") Long id, Answer answer){
+    @PutMapping("/update/{id}")
+    public ResponseModel<Answer> updateAnswer(@PathVariable Long id, @RequestBody Answer answer){
         answer.setId(id);
         return answerService.updateAnswer(answer);
     }
