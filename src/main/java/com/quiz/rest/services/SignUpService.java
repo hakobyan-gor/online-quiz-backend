ca4ee9acbb5c29869abfc2e5f44f466726659bed
Gor Hakobyan <gorhakobiann@gmail.com>
2020-06-01 01:07:00 +0400

    some funtional changes - mail

--- src/main/java/com/quiz/rest/services/SignUpService.java
+++ src/main/java/com/quiz/rest/services/SignUpService.java
@@ -2,12 +2,15 @@ package com.quiz.rest.services;
 
 import com.quiz.models.request.RegistrationRequest;
 import com.quiz.models.response.JwtResponse;
+
+import javax.mail.MessagingException;
+
 import com.quiz.models.ConfirmationToken;
 import com.quiz.models.User;
 
 public interface SignUpService {
 
-    User signUp(RegistrationRequest registrationRequest);
+    User signUp(RegistrationRequest registrationRequest) throws MessagingException;
 
     JwtResponse verifyEMail(ConfirmationToken confirmationToken);
 }
